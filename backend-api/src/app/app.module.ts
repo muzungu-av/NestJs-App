442d0ae0a0a40abfa23d3697017a3927040d6753
Valera <val12042025@gmail.com>
2024-01-21 17:18:22 +0300

    rename/reorg project

--- backend-api/src/app/app.module.ts
+++ backend-api/src/app/app.module.ts
@@ -2,12 +2,12 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { configuration } from 'config/configuration';
 import { validationSchema } from 'config/validation';
-import { ImageModule } from 'src/image/image.module';
+import { ImageModule } from 'image/image.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: `${process.cwd()}/config/env/.env.${process.env.NODE_ENV}`,
+      envFilePath: `${process.cwd()}/src/config/env/.env.${process.env.NODE_ENV}`,
       load: [configuration],
       validationSchema, // Validating the env variables
       isGlobal: true, // environment variables are available to all modules
--- backend-api/src/config/configuration.ts
+++ backend-api/src/config/configuration.ts
@@ -1,3 +1,6 @@
+console.log(`${process.cwd()}`);
+console.log(process.env.NODE_ENV);
+
 export const configuration = () => ({
   NODE_ENV: process.env.NODE_ENV,
   MULTUER_DEST: process.env.MULTER_DEST,
