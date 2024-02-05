408fb8a023f563befde3b820d03bdf968fc9567b
Valera <val12042025@gmail.com>
2024-02-05 11:39:09 +0300

    fix cors

--- backend-api/src/main.ts
+++ backend-api/src/main.ts
@@ -1,11 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 // import { configuration } from 'config/configuration';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, new ExpressAdapter());
+
+  app.enableCors({
+    origin: 'http://localhost:8080', // Замените на разрешенный домен вашего клиента
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+
   await app.listen(3000);
 }
+
 bootstrap();
 
 console.log(`NODE_ENV=` + process.env.NODE_ENV);
