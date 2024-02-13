ed124196dfbd4cb266f5f261fe017e6448fd48b0
Roma2507 <buxarin1999@gmail.com>
2024-02-13 16:22:34 +0600

    add ENV to config

--- frontend/rsbuild.config.ts
+++ frontend/rsbuild.config.ts
@@ -1,6 +1,11 @@
-import { defineConfig } from '@rsbuild/core';
-import { pluginReact } from '@rsbuild/plugin-react';
+import { pluginReact } from "@rsbuild/plugin-react";
+import { defineConfig, loadEnv } from "@rsbuild/core";
+
+const { publicVars } = loadEnv({ prefixes: ["REACT_APP_"] });
 
 export default defineConfig({
   plugins: [pluginReact()],
+  source: {
+    define: publicVars,
+  },
 });
