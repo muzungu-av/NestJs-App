650c445ab2415d7b4aedda4f70fe0f22f82cd25d
Valera <val12042025@gmail.com>
2024-02-23 13:49:20 +0300

    JWT validation enhanced (user verification)

--- backend-api/src/auth/jwt.strategy.ts
+++ backend-api/src/auth/jwt.strategy.ts
@@ -1,19 +1,35 @@
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { configuration } from 'config/configuration';
+import { winstonLogger } from 'winston.logger';
+import { UserService } from 'user/user.service';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor() {
+  constructor(private usersService: UserService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: configuration().SECRET_KEY,
+      secretOrKey: configuration().SECRET_KEY, //todo проверить изменение
     });
   }
 
   async validate(payload: any) {
+    winstonLogger.info(
+      `Validate JWT-token payload: ${JSON.stringify(payload)}`,
+    );
+    if (!payload || !payload.userId || !payload.email) {
+      winstonLogger.warning(`Invalid token payload`);
+      throw new UnauthorizedException('Invalid token payload');
+    }
+    const user = await this.usersService.findOne(payload.email);
+
+    if (!user) {
+      winstonLogger.warning(`Invalid User.email: ${payload.email}`);
+      throw new UnauthorizedException('Invalid token payload');
+    }
+    winstonLogger.info(`User has been validated: ${user}`);
     return { userId: payload.userId, email: payload.login };
   }
 }
