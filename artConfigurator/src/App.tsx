71673014b11b25e8c704d1ab5c25bd8b34617844
Roma2507 <buxarin1999@gmail.com>
2024-03-28 20:14:28 +0100

    feat:list pictures page

--- artConfigurator/src/App.tsx
+++ artConfigurator/src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import "./output.css";
 import { Biography } from "./pages/biography";
+import { Pictures } from "./pages/pictures";
 import LogIn from "./pages/login";
 import MainLayout from "./layouts/MainLayout";
 const App: React.FC = () => {
@@ -12,6 +13,7 @@ const App: React.FC = () => {
         <Routes>
           <Route path="/login" element={<LogIn />} />
           <Route path="/biography" element={<Biography />} />
+          <Route path="/pictures" element={<Pictures />} />
         </Routes>
       </MainLayout>
     </Router>
