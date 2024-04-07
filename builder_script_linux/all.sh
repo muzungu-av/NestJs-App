a110374728038a45e31f041a7ac16c2001a037ab
Valera <val12042025@gmail.com>
2024-04-07 19:03:12 +0300

    linux scripts

---
+++ builder_script_linux/all.sh
@@ -0,0 +1,21 @@
+docker compose stop
+echo "************* BACK **************"
+cd ./backend-api
+rm -r ./dist
+yarn build
+cd ..
+
+echo "*********** ART CONF ************"
+cd ./artConfigurator
+rm -r ./dist
+yarn build:dev
+cd ..
+
+echo "*********** FRONTEND ************"
+cd ./frontend
+rm -r ./dist
+yarn build:dev
+cd ..
+
+echo "************ DOCKER *************"
+docker compose up -d --build
